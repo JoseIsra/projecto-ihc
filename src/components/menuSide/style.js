261ef8f1f53c45f4b259f2cc9bd6f8e4e7bf2539
israel diaz <israelmyname@gmail.com>
2021-04-15 23:48:01 -0500

    problema no se soluciona aun

--- src/components/menuSide/style.js
+++ src/components/menuSide/style.js
@@ -25,17 +25,17 @@ export default makeStyles((theme)=>({
     },
     emergency: {
         display:'none',
-        // [theme.breakpoints.only('xl')]: {
-        //     display:'flex',
-        //     flexWrap:'wrap',
-        //     justifyContent:'center',
-        //     alignItems:'flex-start',
-        //     height:'15rem',
-        //     overflow:'auto',
-        //     '&::-webkit-scrollbar': {
-        //         display: 'none'
-        //     },
-        // }
+        [theme.breakpoints.up('xl')]: {
+            display:'flex',
+            flexWrap:'wrap',
+            justifyContent:'center',
+            alignItems:'flex-start',
+            height:'15rem',
+            overflow:'auto',
+            '&::-webkit-scrollbar': {
+                display: 'none'
+            },
+        }
     },
     text: {
         color: '#666666',

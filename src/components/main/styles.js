18c93fa5d5624cd1c1b8529620733f91f631bb6b
israel diaz <israelmyname@gmail.com>
2021-04-16 23:35:50 -0500

    cambiando largo de papr

--- src/components/main/styles.js
+++ src/components/main/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
         root: {
         width:'60vw',
-        height:'90vh',
+        height:'80vh',
         // [theme.breakpoints.up('lg')]: {
         //         width:'80vw',
         //         height:'70vh'

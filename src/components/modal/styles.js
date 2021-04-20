e38e2b96c41e89960118ab7e10a8a13d0d863b8f
israel diaz <israelmyname@gmail.com>
2021-04-19 23:47:17 -0500

    prueba de nuevo

---
+++ src/components/modal/styles.js
@@ -0,0 +1,37 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const getModalStyle= () => {
+    return {
+        top: '50%',
+        outline:'none',
+        left:'50%', 
+        transform: `translate(-50%, -50%)`,
+    };
+}
+
+export default makeStyles(()=> ({
+    paper: {
+        position: 'absolute',
+        padding:'20px',
+        border: '2px solid orange',
+        borderRadius:'10px',
+        width: '500px',
+        height: '100px',
+        backgroundColor:'#ebebeb',
+        '&: focus': {
+            outline:'none'
+        }
+        
+    },
+    buyButton: {
+        fontSize:'0.7rem',
+        marginTop:'0.5rem',
+        border:'1px solid orange',
+        backgroundColor:'rgba(250, 104, 7, 0.924)',
+        color:'#f7f7f7',
+        fontWeight:'bolder',
+        '&:hover':{
+            backgroundColor:'rgba(250, 104, 7, 0.924)'
+        }
+    }
+}));

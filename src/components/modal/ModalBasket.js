b532d23064db5df758714ee48089733316fd201e
israel diaz <israelmyname@gmail.com>
2021-04-19 23:34:32 -0500

    :rocket: proyecto terminado

---
+++ src/components/modal/ModalBasket.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import useStyles, { getModalStyle } from './style';
+import {Modal, Button, Typography } from '@material-ui/core'
+import { useDataLayerValue } from '../../datalayer/DataLayer';
+
+
+export const ModalBasket = () => {
+    const classes = useStyles();
+    const modalStyle = getModalStyle();
+    const [open, setOpen] = useState(false);
+    const [, dispatch ] = useDataLayerValue();
+
+
+    const sleep = ()=> {
+        return new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+
+    
+
+    useEffect(()=>{
+        let timeout = setTimeout(()=> {
+            setOpen(false);   
+        }, 2500);
+        
+        return () => clearTimeout(timeout);
+        
+    }, [open])
+
+    
+
+    const handleOpen = async () => {
+        setOpen(true);
+        await sleep();
+        dispatch({type:'CLEAR__BASKET'})
+    };
+    
+    
+    const handleClose = () => {
+        setOpen(false);
+        dispatch({type:'CLEAR__BASKET'})
+    };
+
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+            <Typography variant="body1">Su pedido ha sido registrado,
+            espere a que lo llamen
+            </Typography>
+        </div>
+    );
+
+    return (
+        <div>
+        <Button className={classes.button}   onClick={handleOpen} >
+        Realizar compra
+        </Button>
+
+        <Modal 
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        >
+            { body }
+        </Modal>
+        </div>
+    )
+}
